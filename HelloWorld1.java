09915fa740ee80ea81a5d3f5a11b1a35688c7db8
last-devil <2019bcs110@sggs.ac.in>
2025-04-29 11:35:17 +0530

    Restored and modified HelloWorld1.java from first commit

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -1,9 +1,6 @@
 public class HelloWorld1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World2");
-        System.out.println("Hello, World3");
-        System.out.println("Hello, World4");
-
+        System.out.println("this message is after the adding some new thing");
     }
 }
